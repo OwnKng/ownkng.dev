9bf72bfdcac7acdd25731da73c25287b9f4e3c42
OwnKng <owen.king@outlook.com>
2020-12-27 18:05:57 +0000

    Added hero

--- components/styled/element/Main.js
+++ components/styled/element/Main.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  width: 85%;
+  width: 90%;
   min-height: 100vh;
   margin: 0px auto;
   padding: 4rem 1rem 0;
--- components/styled/layout/Hero.js
+++ components/styled/layout/Hero.js
@@ -1,40 +1,80 @@
 import styled from "styled-components";
 import { motion } from "framer-motion";
+import Voronoi from "../../visx/Voronoi";
+import ParentSize from "@visx/responsive/lib/components/ParentSize";
 
 const Hero = ({ className }) => {
   return (
     <div className={className}>
-      <div>
-        <motion.h1
-          initial={{ y: 50 }}
-          animate={{ y: 0 }}
-          transition={{ type: "linear" }}
-        >
-          Owen King
-        </motion.h1>
-      </div>
+      <NameCard />
+      <ParentSize>
+        {({ width, height }) => <Voronoi width={width} height={height} />}
+      </ParentSize>
     </div>
   );
 };
 
 export default styled(Hero)`
-  height: 60vh;
+  height: 80vh;
   width: 100%;
   border: 1px solid black;
   position: relative;
+`;
 
-  div {
+const StyledNameCard = styled(motion.div)`
     position: absolute;
     top: 40%;
     width: 100%;
-    text-align: center;
-    border-top: 1px solid black;
-    border-bottom: 1px solid black;
-    overflow: hidden;
 
     h1 {
-      margin: 0px;
-      font-size: 4rem;
+        margin: 0px 10px;
+        font-size: 4rem;
+      }
+
+      h2 {
+        margin: 10px 10px;
+        font-size: 2rem;
+      }
+
+    .title {
+        overflow: hidden;
+        border-top: 1px solid black;
+        border-bottom: 1px solid black;
     }
-  }
-`;
+
+    .subtitle {
+      overflow: hidden;
+      border-bottom: 1px solid black;
+    }
+  }`;
+
+const divVariants = {
+  initial: {},
+  animate: {
+    transition: {
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const NameCard = () => {
+  const titleVariants = {
+    initial: { y: 100 },
+    animate: { y: 0 },
+    transition: { type: "easeIn" },
+  };
+  return (
+    <StyledNameCard variants={divVariants} initial='initial' animate='animate'>
+      <div className='title'>
+        <motion.h1 variants={titleVariants} transition='transition'>
+          OWEN KING
+        </motion.h1>
+      </div>
+      <div className='subtitle'>
+        <motion.h2 variants={titleVariants} transition='transition'>
+          I USE MODERN TECH TO ANALYSE, VISUALISE AND MODEL DATA
+        </motion.h2>
+      </div>
+    </StyledNameCard>
+  );
+};
